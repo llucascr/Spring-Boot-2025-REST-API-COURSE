ddba6bab94e604619a43719c71fd6f61792d8f6b
llucascr <lucasdecamposranzani@gmail.com>
2025-12-06 13:25:08 -0300

    47. 0603 Retornando Listas de Objetos em JSON

--- rest-with-spring-boot-and-java/src/main/java/br/com/llucascr/services/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/llucascr/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.llucascr.services;
 import br.com.llucascr.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,18 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+
+        //Mock
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
@@ -25,6 +39,15 @@ public class PersonServices {
         person.setAddress("Cosmpolis - SP - Brasil");
         person.setGender("Male");
 
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setFirstName("FirstName" + i);
+        person.setLastName("LastName" + i);
+        person.setAddress("Some Address in Brasil");
+        person.setGender("Male");
         return person;
     }
 
